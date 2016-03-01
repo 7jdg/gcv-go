18cdba347069d84a7a7dca709452c62dcb141858
Slavomir <gagliardetto@users.noreply.github.com>
2016-03-01 21:25:57 +0100

    Initial version

--- gcvgo.go
+++ gcvgo.go
@@ -1,7 +1,15 @@
 package gcvgo
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"strconv"
 )
 
 // NewClient creates a new API client with the provided credentials
@@ -13,20 +21,99 @@ func NewClient(credentials Credentials) (Client, error) {
 	}, nil
 }
 
-func (reqs Requests) Add(request Request) {
-	reqs = append(reqs, request)
+func (reqs *Requests) Add(request Request) {
+	*reqs = append(*reqs, request)
 }
 
 func (request *Request) AddImageFromBase64(base64Content string) {
 	request.Image.Content = base64Content
 }
 
-func (features Features) Add(featureType FeatureType, MaxResults int64) {
-	features = append(features, Feature{
+func (features *Features) Add(featureType FeatureType, MaxResults int64) {
+	*features = append(*features, Feature{
 		Type:       featureType,
 		MaxResults: MaxResults,
 	})
 }
 
-func (client *Client) Do(reqs Requests) {
+func (client *Client) Do(reqs Requests) ([]Response, error) {
+	payload := struct {
+		Requests Requests `json:"requests"`
+	}{
+		Requests: reqs,
+	}
+	JSONPayload, err := json.Marshal(payload)
+	if err != nil {
+		return []Response{}, err
+	}
+	fmt.Println(string(JSONPayload))
+	response, _, err := client.fetchAndReturnPage(JSONPayload)
+
+	fmt.Println(string(response))
+
+	var result struct {
+		Responses []Response `json:"responses"`
+	}
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return []Response{}, err
+	}
+
+	return result.Responses, nil
+}
+
+func (client *Client) fetchAndReturnPage(body []byte) ([]byte, http.Header, error) {
+
+	domain := fmt.Sprintf("https://vision.googleapis.com/v1/images:annotate?key=%s", client.Credentials.APIkey)
+	requestURL, err := url.Parse(domain)
+	if err != nil {
+		return []byte(""), http.Header{}, err
+	}
+	requestURL.Path = "/v1/images:annotate"
+
+	fmt.Println(requestURL)
+
+	request, err := http.NewRequest("POST", requestURL.String(), bytes.NewBuffer(body))
+	if err != nil {
+		return []byte(""), http.Header{}, fmt.Errorf("Failed to get the URL %s: %s", requestURL, err)
+	}
+	request.Header.Add("Content-Length", strconv.Itoa(len(body)))
+
+	request.Header.Add("Connection", "Keep-Alive")
+	request.Header.Add("Accept-Encoding", "gzip, deflate")
+	request.Header.Add("Content-Type", "application/json")
+	//request.Header.Add("Authorization", "Bearer "+client.Credentials.APIkey)
+
+	response, err := client.httpClient.Do(request)
+	if err != nil {
+		return []byte(""), http.Header{}, fmt.Errorf("Failed to get the URL %s: %s", requestURL, err)
+	}
+	defer response.Body.Close()
+
+	var responseReader io.ReadCloser
+	switch response.Header.Get("Content-Encoding") {
+	case "gzip":
+		decompressedBodyReader, err := gzip.NewReader(response.Body)
+		if err != nil {
+			return []byte(""), http.Header{}, err
+		}
+		responseReader = decompressedBodyReader
+		defer responseReader.Close()
+	default:
+		responseReader = response.Body
+	}
+
+	responseBody, err := ioutil.ReadAll(responseReader)
+	if err != nil {
+		return []byte(""), http.Header{}, err
+	}
+
+	if response.StatusCode > 299 || response.StatusCode < 199 {
+		fmt.Println(response.StatusCode)
+		fmt.Println(string(responseBody))
+
+		return []byte(""), http.Header{}, err
+	}
+
+	return responseBody, response.Header, nil
 }
--- structures.go
+++ structures.go
@@ -15,29 +15,23 @@ type Client struct {
 
 type Request struct {
 	Image struct {
-		Content string `json:"content"`
+		Content string `json:"content,omitempty"`
 
-		Source struct {
-			GcsImageUri string `json:"gcsImageUri"`
-		} `json:"source"`
+		Source *struct {
+			GcsImageUri string `json:"gcsImageUri,omitempty"`
+		} `json:"source,omitempty"`
 	} `json:"image,omitempty"`
 
 	Features Features `json:"features,omitempty"`
 
 	ImageContext struct {
 		LatLongRect struct {
-			MinLatLng struct {
-				Latitude  float64 "latitude"  // The latitude in degrees. It must be in the range [-90.0, +90.0].
-				Longitude float64 "longitude" // The longitude in degrees. It must be in the range [-180.0, +180.0].
-			} "minLatLng"
+			MinLatLng LatLng `json:"minLatLng,omitempty"`
 
-			MaxLatLng struct {
-				Latitude  float64 "latitude"  // The latitude in degrees. It must be in the range [-90.0, +90.0].
-				Longitude float64 "longitude" // The longitude in degrees. It must be in the range [-180.0, +180.0].
-			} "maxLatLng"
-		} `json:"latLongRect"`
+			MaxLatLng LatLng `json:"maxLatLng,omitempty"`
+		} `json:"latLongRect,omitempty"`
 
-		LanguageHints []string `json:"languageHints"` // for a complete list: https://cloud.google.com/translate/v2/using_rest#language-params
+		LanguageHints []string `json:"languageHints,omitempty"` // for a complete list: https://cloud.google.com/translate/v2/using_rest#language-params
 	} `json:"imageContext,omitempty"`
 }
 
@@ -69,4 +63,187 @@ const (
 
 //
 
-type Response struct{}
+type Response struct {
+	FaceAnnotations []FaceAnnotation `json:"faceAnnotations,omitempty"`
+
+	LandmarkAnnotations []EntityAnnotation `json:"landmarkAnnotations,omitempty"`
+
+	LogoAnnotations []EntityAnnotation `json:"logoAnnotations,omitempty"`
+
+	LabelAnnotations []EntityAnnotation `json:"labelAnnotations,omitempty"`
+
+	TextAnnotations []EntityAnnotation `json:"textAnnotations,omitempty"`
+
+	SafeSearchAnnotation SafeSearchAnnotation `json:"safeSearchAnnotation,omitempty"`
+
+	ImagePropertiesAnnotation ImageProperties `json:"imagePropertiesAnnotation,omitempty"`
+
+	Error Status `json:"error,omitempty"`
+}
+
+type Vertex struct {
+	X int64 `json:"x"`
+	Y int64 `json:"y"`
+}
+
+type Landmark struct {
+	Type     FaceLandmarkType `json:"type"`
+	Position Position         `json:"position"`
+}
+
+type FaceLandmarkType string
+
+const (
+	UNKNOWN_LANDMARK             FaceLandmarkType = "UNKNOWN_LANDMARK"             // Unknown face landmark detected. Should not be filled.
+	LEFT_EYE                     FaceLandmarkType = "LEFT_EYE"                     // Left eye.
+	RIGHT_EYE                    FaceLandmarkType = "RIGHT_EYE"                    // Right eye.
+	LEFT_OF_LEFT_EYEBROW         FaceLandmarkType = "LEFT_OF_LEFT_EYEBROW"         // Left of left eyebrow.
+	RIGHT_OF_LEFT_EYEBROW        FaceLandmarkType = "RIGHT_OF_LEFT_EYEBROW"        // Right of left eyebrow.
+	LEFT_OF_RIGHT_EYEBROW        FaceLandmarkType = "LEFT_OF_RIGHT_EYEBROW"        // Left of right eyebrow.
+	RIGHT_OF_RIGHT_EYEBROW       FaceLandmarkType = "RIGHT_OF_RIGHT_EYEBROW"       // Right of right eyebrow.
+	MIDPOINT_BETWEEN_EYES        FaceLandmarkType = "MIDPOINT_BETWEEN_EYES"        // Midpoint between eyes.
+	NOSE_TIP                     FaceLandmarkType = "NOSE_TIP"                     // Nose tip.
+	UPPER_LIP                    FaceLandmarkType = "UPPER_LIP"                    // Upper lip.
+	LOWER_LIP                    FaceLandmarkType = "LOWER_LIP"                    // Lower lip.
+	MOUTH_LEFT                   FaceLandmarkType = "MOUTH_LEFT"                   // Mouth left.
+	MOUTH_RIGHT                  FaceLandmarkType = "MOUTH_RIGHT"                  // Mouth right.
+	MOUTH_CENTER                 FaceLandmarkType = "MOUTH_CENTER"                 // Mouth center.
+	NOSE_BOTTOM_RIGHT            FaceLandmarkType = "NOSE_BOTTOM_RIGHT"            // Nose, bottom right.
+	NOSE_BOTTOM_LEFT             FaceLandmarkType = "NOSE_BOTTOM_LEFT"             // Nose, bottom left.
+	NOSE_BOTTOM_CENTER           FaceLandmarkType = "NOSE_BOTTOM_CENTER"           // Nose, bottom center.
+	LEFT_EYE_TOP_BOUNDARY        FaceLandmarkType = "LEFT_EYE_TOP_BOUNDARY"        // Left eye, top boundary.
+	LEFT_EYE_RIGHT_CORNER        FaceLandmarkType = "LEFT_EYE_RIGHT_CORNER"        // Left eye, right corner.
+	LEFT_EYE_BOTTOM_BOUNDARY     FaceLandmarkType = "LEFT_EYE_BOTTOM_BOUNDARY"     // Left eye, bottom boundary.
+	LEFT_EYE_LEFT_CORNER         FaceLandmarkType = "LEFT_EYE_LEFT_CORNER"         // Left eye, left corner.
+	RIGHT_EYE_TOP_BOUNDARY       FaceLandmarkType = "RIGHT_EYE_TOP_BOUNDARY"       // Right eye, top boundary.
+	RIGHT_EYE_RIGHT_CORNER       FaceLandmarkType = "RIGHT_EYE_RIGHT_CORNER"       // Right eye, right corner.
+	RIGHT_EYE_BOTTOM_BOUNDARY    FaceLandmarkType = "RIGHT_EYE_BOTTOM_BOUNDARY"    // Right eye, bottom boundary.
+	RIGHT_EYE_LEFT_CORNER        FaceLandmarkType = "RIGHT_EYE_LEFT_CORNER"        // Right eye, left corner.
+	LEFT_EYEBROW_UPPER_MIDPOINT  FaceLandmarkType = "LEFT_EYEBROW_UPPER_MIDPOINT"  // Left eyebrow, upper midpoint.
+	RIGHT_EYEBROW_UPPER_MIDPOINT FaceLandmarkType = "RIGHT_EYEBROW_UPPER_MIDPOINT" // Right eyebrow, upper midpoint.
+	LEFT_EAR_TRAGION             FaceLandmarkType = "LEFT_EAR_TRAGION"             // Left ear tragion.
+	RIGHT_EAR_TRAGION            FaceLandmarkType = "RIGHT_EAR_TRAGION"            // Right ear tragion.
+	LEFT_EYE_PUPIL               FaceLandmarkType = "LEFT_EYE_PUPIL"               // Left eye pupil.
+	RIGHT_EYE_PUPIL              FaceLandmarkType = "RIGHT_EYE_PUPIL"              // Right eye pupil.
+	FOREHEAD_GLABELLA            FaceLandmarkType = "FOREHEAD_GLABELLA"            // Forehead glabella.
+	CHIN_GNATHION                FaceLandmarkType = "CHIN_GNATHION"                // Chin gnathion.
+	CHIN_LEFT_GONION             FaceLandmarkType = "CHIN_LEFT_GONION"             // Chin left gonion.
+	CHIN_RIGHT_GONION            FaceLandmarkType = "CHIN_RIGHT_GONION"            //
+)
+
+//
+
+type Position struct {
+	X int64 `json:"x"`
+	Y int64 `json:"y"`
+	Z int64 `json:"z"`
+}
+
+//
+
+type Likelihood string
+
+const (
+	UNKNOWN       Likelihood = "UNKNOWN"       // Unknown likelihood.
+	VERY_UNLIKELY Likelihood = "VERY_UNLIKELY" // The image very unlikely belongs to the vertical specified.
+	UNLIKELY      Likelihood = "UNLIKELY"      // The image unlikely belongs to the vertical specified.
+	POSSIBLE      Likelihood = "POSSIBLE"      // The image possibly belongs to the vertical specified.
+	LIKELY        Likelihood = "LIKELY"        // The image likely belongs to the vertical specified.
+	VERY_LIKELY   Likelihood = "VERY_LIKELY"   // The image very likely belongs to the vertical specified.
+)
+
+//
+
+type LocationInfo struct {
+	LatLng LatLng `json:"latLng"`
+}
+
+type LatLng struct {
+	Latitude  float64 `json:"latitude,omitempty"`  // The latitude in degrees. It must be in the range [-90.0, +90.0].
+	Longitude float64 `json:"longitude,omitempty"` // The longitude in degrees. It must be in the range [-180.0, +180.0].
+}
+
+type Property struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+//
+
+type FaceAnnotation struct {
+	BoundingPoly struct {
+		Vertices []Vertex `json:"vertices"`
+	} `json:"boundingPoly"`
+	FdBoundingPoly struct {
+		Vertices []Vertex `json:"vertices"`
+	} `json:"fdBoundingPoly"`
+	Landmarks             []Landmark `json:"landmarks"`
+	RollAngle             float64    `json:"rollAngle"`
+	PanAngle              float64    `json:"panAngle"`
+	TiltAngle             float64    `json:"tiltAngle"`
+	DetectionConfidence   float64    `json:"detectionConfidence"`
+	LandmarkingConfidence float64    `json:"landmarkingConfidence"`
+
+	JoyLikelihood          Likelihood `json:"joyLikelihood"`
+	SorrowLikelihood       Likelihood `json:"sorrowLikelihood"`
+	AngerLikelihood        Likelihood `json:"angerLikelihood"`
+	SurpriseLikelihood     Likelihood `json:"surpriseLikelihood"`
+	UnderExposedLikelihood Likelihood `json:"underExposedLikelihood"`
+	BlurredLikelihood      Likelihood `json:"blurredLikelihood"`
+	HeadwearLikelihood     Likelihood `json:"headwearLikelihood"`
+}
+
+type EntityAnnotation struct {
+	Mid          string  `json:"mid"`
+	Locale       string  `json:"locale"`
+	Description  string  `json:"description"`
+	Score        float64 `json:"score"`
+	Confidence   float64 `json:"confidence"`
+	Topicality   float64 `json:"topicality"`
+	BoundingPoly struct {
+		Vertices []Vertex `json:"vertices"`
+	} `json:"boundingPoly"`
+	Locations  []LocationInfo `json:"locations"`
+	Properties []Property     `json:"properties"`
+}
+
+type SafeSearchAnnotation struct {
+	Adult    Likelihood `json:"adult"`
+	Spoof    Likelihood `json:"spoof"`
+	Medical  Likelihood `json:"medical"`
+	Violence Likelihood `json:"violence"`
+}
+
+type ImageProperties struct {
+	DominantColors DominantColorsAnnotation `json:"dominantColors"`
+}
+
+type DominantColorsAnnotation struct {
+	Colors []ColorInfo `json:"colors"`
+}
+
+type ColorInfo struct {
+	Color         Color   `json:"color"`
+	Score         float64 `json:"score"`
+	PixelFraction float64 `json:"pixelFraction"`
+}
+
+type Color struct {
+	Red   float64 `json:"red"`
+	Green float64 `json:"green"`
+	Blue  float64 `json:"blue"`
+	Alpha float64 `json:"alpha"`
+}
+
+//
+
+type Status struct {
+	Code    int64    `json:"code"`
+	Message string   `json:"message"`
+	Details []Detail `json:"details"`
+}
+
+type Detail struct {
+	Id   int64  `json:"id"`
+	Type string `json:"@type"`
+}
